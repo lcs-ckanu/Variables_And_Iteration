978115a02049b0ac51773ca17829e785205b8622
Kanu, Chinedum <ckanu@lcs-mba3120.lcs.on.ca>
2017-10-12 15:21:49 -0400

    Making progress in drawing triangles.

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -17,8 +17,24 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 //: ## Add your code below
+//draw Circles
+for x in stride(from: 0, through: 300, by: 30)  {
+    for y in stride(from: 0, to: 300, by: 30)  {
+        canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
+    }
+}
+// Draw first rectangle
+canvas.fillColor = Color.init(hue: 30, saturation: 0, brightness: 25, alpha: 90)
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 700, height: 300)
 
+// DraW second Rectangle
+canvas.fillColor = Color.init(hue: 30 , saturation: 0 , brightness: 30, alpha: 100)
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 700, height: 100)
 
+// Draw Triaangle
+for i in 1...50 {
+    canvas.drawLine(fromX: i, fromY: 0, toX: i, toY: 200-i)
+}
 
 /*:
  ## Template code
